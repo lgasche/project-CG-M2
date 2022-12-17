f0eda33798dd6ddac8c6ec491c24b333f4d05e67
luca <luca.gasche@gmail.com>
2022-12-17 21:14:42 +0100

    feat: update for Treasure

--- src/LvlReader.cpp
+++ src/LvlReader.cpp
@@ -1,13 +1,27 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+
 using namespace std;
 
 #include "LvlReader.hpp"
+#include "Treasure.cpp"
+
+vector<string> LvlReader::parse_line(const string str)
+{
+    stringstream ss(str);
+    string token;
+    vector<string> res;
+    while (!ss.eof())
+    {
+        getline(ss, token, ':');
+        res.push_back(token);
+    }
+    return res;
+}
 
 void LvlReader::build()
 {
-    cout << path_file << endl;
     string line;
     ifstream file(path_file);
     if (!file)
@@ -16,21 +30,26 @@ void LvlReader::build()
     }
     else
     {
-        cout << "here" << endl;
         getline(file, line);
         first_comment = line;
 
         getline(file, line);
         path_dungeon_ppm = line;
 
+        // Part treasures.
         getline(file, line);
         nb_treasures = int(stoi(line));
         for (int i = 0; i < nb_treasures; i++)
         {
-            // Todo add treasure
+            // Parse data in line correspoding to the Treasure.
             getline(file, line);
+            auto tokens = parse_line(line);
+            auto treasure = Treasure(stoul(tokens[0]), make_tuple(stoul(tokens[1]), stoul(tokens[2])), tokens[3]);
+            std::cout << treasure << std::endl;
+            treasures.push_back(treasure);
         }
 
+        // Part monstrers.
         getline(file, line);
         nb_monsters = int(stoi(line));
         for (int i = 0; i < nb_monsters; i++)
--- src/LvlReader.hpp
+++ src/LvlReader.hpp
@@ -3,36 +3,41 @@
 #include <filesystem>
 #include <iostream>
 #include <string>
+#include <vector>
+#include <memory>
+#include <experimental/filesystem>
 
 using namespace std;
+namespace fs = std::experimental::filesystem;
+
+#include "Treasure.hpp"
 
 class LvlReader
 {
+    friend std::ostream &operator<<(std::ostream &stream, const LvlReader &lvlReader)
+    {
+        return stream << "LvlReader: " << lvlReader.file_name << " | " << lvlReader.first_comment
+                      << " | " << lvlReader.path_dungeon_ppm << " | " << lvlReader.nb_monsters;
+    }
+
 public:
     string file_name;
     LvlReader(const string file_name_) : file_name{file_name_}
     {
-        path_file = filesystem::current_path() / "lvls" / file_name += ".txt";
+        path_file = fs::current_path() / "lvls" / file_name += ".txt";
     }
 
+    vector<string> parse_line(const string str);
     void build();
 
-    void get_infos()
-    {
-        cout << "LvlReader : " << file_name << endl;
-        cout << "LvlReader : " << first_comment << endl;
-        cout << "LvlReader : " << path_dungeon_ppm << endl;
-        cout << "LvlReader : " << nb_treasures << endl;
-        cout << "LvlReader : " << nb_monsters << endl;
-    };
-
 private:
-    filesystem::path path_file;
+    fs::path path_file;
 
     string first_comment;
     string path_dungeon_ppm;
     int nb_treasures;
-    // vector treasures.
+    // vector<unique_ptr<Treasure>> treasures;
+    vector<Treasure> treasures;
     int nb_monsters;
     // vector monsters;
 };
